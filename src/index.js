ceee7aa42b5588286c4176d2d1ebb700b4668aa4
Md Asif Alam <mdasifalam910@gmail.com>
2023-09-25 12:56:09 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Crads";
+
+ReactDOM.render(
+  <>
+    <Card
+      imgsrc="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABYooBTHs4N3UkCepoM76TAfw7YkFKO37GzRzyticAbdjKNHKkfnkDFc9riKg78FkOzlWCjZp_WTRg2n8HaEiYyIzAK453YTVWl-u.jpg?r=077"
+      title="A Netflix Original Series"
+      sname="DARK"
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card
+      imgsrc="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABS6v2gvwesuRN6c28ZykPq_fpmnQCJwELBU-kAmEcuC9HhWX-DfuDbtA-bfo-IrfgNtxl0qwJJlhI6DENsGFXknKkjhxPGTV-qhp.jpg?r=608"
+      title="A Netflix Original Series"
+      sname="Stranger Things"
+      link="https://www.netflix.com/in/title/80057281"
+    />
+    <Card
+      imgsrc="https://i.dawn.com/large/2020/05/5ec697a9ce9b4.jpg"
+      title="A Netflix Original Series"
+      sname="Money Heist"
+      link="https://www.netflix.com/in/title/80192098"
+    />
+    <Card
+      imgsrc="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABalQ_eRkQ0v0IJMSMae-_5JcV7X6TC9vuZOYm7cIQ4u_McUvV7T6xVxltAN05h--99m-pcfKjFekD9j1aip6LwNo48M7SFnQc7n-.jpg?r=a1a"
+      title="A Netflix Original Series"
+      sname="The Silent Sea"
+      link="https://www.netflix.com/in/title/81098012"
+    />
+  </>,
+  document.getElementById("root")
+);
